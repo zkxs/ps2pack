703faa9ff3b79c4c72c148288e5ef40a883c30dd
Michael Ripley <zkxs00@gmail.com>
2016-04-12 20:26:38 -0500

    Removed virtual filesystem stuff (it was a bad idea)
    The self test now read metadata in ALL pack files, but doesn't print
    anything because it takes a rather long time to print.

--- src/com/github/zkxs/ps2pack/SimpleTest.java
+++ src/com/github/zkxs/ps2pack/SimpleTest.java
@@ -1,6 +1,10 @@
 package com.github.zkxs.ps2pack;
 
 import java.io.IOException;
+import java.nio.file.DirectoryStream;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 /**
  * A simple test for the current state of the API.
@@ -9,20 +13,36 @@ import java.io.IOException;
  */
 
 public class SimpleTest
-{	
+{
+	private final static String ASSETS_PATH = "C:\\Program Files (x86)\\Steam\\steamapps\\common\\PlanetSide 2\\Resources\\Assets";
+	
 	public static void main(String[] args)
 	{
 		try
 		{
-			PackFile pf = new PackFile(
-				"C:\\Program Files (x86)\\Steam\\steamapps\\common\\PlanetSide 2\\Resources\\Assets\\Assets_000.pack"
-			);
-			pf.getObjects().forEach(System.out::println);
+			DirectoryStream<Path> ds = Files.newDirectoryStream(Paths.get(ASSETS_PATH), p -> {
+				String filename = p.getFileName().toString();
+				return filename.startsWith("Assets_") && filename.endsWith(".pack");
+			});
+			ds.forEach(p -> {
+				try
+				{
+					PackFile pf = new PackFile(p);
+					//pf.getObjects().forEach(System.out::println);
+				}
+				catch (IOException e)
+				{
+					e.printStackTrace();
+					System.exit(1);
+				}
+			});
 		}
 		catch (IOException e)
 		{
 			e.printStackTrace();
 			System.exit(1);
 		}
+		
+		System.out.println("done");
 	}
 }
--- src/com/github/zkxs/ps2pack/filesystem/package-info.java
+++
@@ -1,5 +0,0 @@
-/**
- * Classes implementing the virtual filesystem used for browsing objects contained within a set of .pack files
- * @author Michael Ripley (<a href="mailto:zkxs00@gmail.com">zkxs00@gmail.com</a>) Apr 12, 2016
- */
-package com.github.zkxs.ps2pack.filesystem;
